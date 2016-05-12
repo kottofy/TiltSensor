ea6b6710d433f47736608b0d2ad45fda1f86df7d
Kristin Ottofy <kottofy@outlook.com>
2016-05-12 10:48:30 -0400

    removing template file for easier local_settings

---
+++ TiltSensor/lcoal_settings.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * This is a secret place for the connection string to an IOT device.
+ * It is recommended to keep this file and connection string private for security reasons.
+ * 
+ * 
+ * 1. Paste in your device connection string in the ""
+ * 2. Save the file 
+ * 
+ * */
+
+
+class local_settings
+{
+    const string deviceConnectionString = "";
+
+    public static string getDeviceConnectionString()
+    {
+        return deviceConnectionString;
+    }
+}
